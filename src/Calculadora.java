54659bed373307ff74ce1251e776d0468f2b126d
Dani163d <Danielasilvacastillo163@gmail.com>
2025-10-30 17:19:30 -0400

    funciones y tipo de datos double

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,46 +5,15 @@ public class Calculadora {
         Scanner consola = new Scanner(System.in);
         while (true) {
             System.out.println("Aplicacion Calculadora");
-            //        mostrar el menu
-            System.out.println("""
-                    1. Suma
-                    2. Resta
-                    3. Multiplicacion
-                    4. Division
-                    5. Salir
-                    """);
-            System.out.print("Seleccione la operacion a realizar: ");
+//        mostrar el menu
+            mostrarMenu();
             try {
-
-
                 var operacion = Integer.parseInt(consola.nextLine());
                 //        revisar que este dentro de las opciones mencionadas
                 if (operacion >= 1 && operacion <= 4) {
-                    System.out.print("Proporciona el valor del operando1: ");
-                    var operando1 = Integer.parseInt(consola.nextLine());
-                    System.out.print("Proporciona el valor del operando2: ");
-                    var operando2 = Integer.parseInt(consola.nextLine());
-                    int resultado;
-                    switch (operacion) {
-                        case 1 -> {
-                            resultado = operando1 + operando2;
-                            System.out.println("resultado de la suma:  = " + resultado);
-                        }
-                        case 2 -> {
-                            resultado = operando1 - operando2;
-                            System.out.println("resultado de la resta:  = " + resultado);
-                        }
-                        case 3 -> {
-                            resultado = operando1 * operando2;
-                            System.out.println("resultado de la Multiplicacion:  = " + resultado);
-                        }
-                        case 4 -> {
-                            resultado = operando1 / operando2;
-                            System.out.println("resultado de la division:  = " + resultado);
-                        }
-                        default -> System.out.println("Opcion erronea: " + operacion);
+//              llamamos la funcion de ejecutarOperacion
+                    ejecutarFunciones(operacion, consola);
 
-                    }
                 } else if (operacion == 5) {
                     System.out.println("Saliendo...");
                     break;
@@ -59,4 +28,44 @@ public class Calculadora {
             }
         } // fin del while
     } // fin de main
+
+    private static void mostrarMenu(){
+//        mostrar el menu
+        System.out.println("""
+                    1. Suma
+                    2. Resta
+                    3. Multiplicacion
+                    4. Division
+                    5. Salir
+                    """);
+        System.out.print("Seleccione la operacion a realizar: ");
+    }
+
+    private static void ejecutarFunciones(int operacion, Scanner consola) {
+        System.out.print("Proporciona el valor del operando1: ");
+        var operando1 = Double.parseDouble(consola.nextLine());
+        System.out.print("Proporciona el valor del operando2: ");
+        var operando2 = Double.parseDouble(consola.nextLine());
+        Double resultado;
+        switch (operacion) {
+            case 1 -> {
+                resultado = operando1 + operando2;
+                System.out.println("resultado de la suma:  = " + resultado);
+            }
+            case 2 -> {
+                resultado = operando1 - operando2;
+                System.out.println("resultado de la resta:  = " + resultado);
+            }
+            case 3 -> {
+                resultado = operando1 * operando2;
+                System.out.println("resultado de la Multiplicacion:  = " + resultado);
+            }
+            case 4 -> {
+                resultado = operando1 / operando2;
+                System.out.println("resultado de la division:  = " + resultado);
+            }
+            default -> System.out.println("Opcion erronea: " + operacion);
+        }
+    }
+
 } // fin de clase
